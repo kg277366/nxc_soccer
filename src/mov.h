3d1261d0fc822d35ece9eb18affc204592211fb9
Karol Grzybowski <kg277366@students.mimuw.edu.pl>
2012-05-08 17:04:45 +0200

    reset works due to wait

--- src/mov.h
+++ src/mov.h
@@ -1,27 +1,32 @@
 // Movement utilities
-byte MOV_outputs[2] = {OUT_DRIVE_L, OUT_DRIVE_R};
 void rotate1w (int deg) {
-     if (deg > 0)
+     if (deg >= 0)
           RotateMotorPID(OUT_DRIVE_R, 100, deg * MM2DEG(PI * TRACK_FRONT_MM) / 180, PID_P, PID_I, PID_D);
      else
           RotateMotorPID(OUT_DRIVE_L, 100, -deg * MM2DEG(PI * TRACK_FRONT_MM) / 180, PID_P, PID_I, PID_D);
-     ResetAllTachoCounts(MOV_outputs);
+     ResetAllTachoCounts(OUT_DRIVE);
+     Wait(10);
 }
 
 void rotate2w (int deg) {
-     RotateMotorExPID(MOV_outputs, 100, deg * MM2DEG(PI * TRACK_FRONT_MM) / 360, 100, true, true, PID_P, PID_I, PID_D);
+     byte outputs[2] = {OUT_DRIVE_L, OUT_DRIVE_R};
+     RotateMotorExPID(outputs, 100, deg * MM2DEG(PI * TRACK_FRONT_MM) / 360, 100, true, true, PID_P, PID_I, PID_D);
+     ResetAllTachoCounts(OUT_DRIVE);
+     Wait(10);
 }
 
 void run (int mm) {
-	RotateMotorExPID(MOV_outputs, 100, MM2DEG(mm), 0, true, true, PID_P, PID_I, PID_D);
+  byte outputs[2] = {OUT_DRIVE_L, OUT_DRIVE_R};
+  RotateMotorExPID(outputs, 100, MM2DEG(mm), 0, true, true, PID_P, PID_I, PID_D);
 }
 
 void kick () {
-	RotateMotorPID(OUT_KICK, 100, 90, PID_P, PID_I, PID_D);
 	RotateMotorPID(OUT_KICK, 100, -90, PID_P, PID_I, PID_D);
+	RotateMotorPID(OUT_KICK, 100, 90, PID_P, PID_I, PID_D);
 }
 
 void MOV_init() {
 	ResetAllTachoCounts(OUT_KICK);
+	Wait(10);
 }
 
